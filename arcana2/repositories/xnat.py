6291b237e9a94f90124031f86d6df6247b32107a
Tom Close <tom.g.close@gmail.com>
2021-10-12 08:38:53 +1100

    moved xnat docker archive dir into bound mount

--- arcana2/repositories/xnat.py
+++ arcana2/repositories/xnat.py
@@ -25,7 +25,6 @@ from arcana2.core.data.set import Dataset
 from arcana2.dataspaces.clinical import Clinical
 
 
-
 logger = logging.getLogger('arcana2')
 
 special_char_re = re.compile(r'[^a-zA-Z_0-9]')
@@ -437,7 +436,6 @@ class Xnat(Repository):
                     path=self.unescape_name(xresource.label),
                     uris={xresource.format: xresource.uri})
 
-
     def dicom_header(self, file_group):
         def convert(val, code):
             if code == 'TM':
@@ -607,6 +605,7 @@ class Xnat(Repository):
 
     @classmethod
     def node_name(cls, data_node):
+        """"""
         if data_node.frequency not in (Clinical.subject, Clinical.session):
             node_name = (
                 '__' + '__'.join(
@@ -616,35 +615,35 @@ class Xnat(Repository):
             node_name = data_node.id
         return node_name
 
-    @classmethod
-    def unescape_name(cls, xname: str):
-        """Reverses the escape of an item name by `escape_name`
+    # @classmethod
+    # def unescape_name(cls, xname: str):
+    #     """Reverses the escape of an item name by `escape_name`
 
-        Parameters
-        ----------
-        xname : `str`
-            An escaped name of a data node stored in the project resources
+    #     Parameters
+    #     ----------
+    #     xname : `str`
+    #         An escaped name of a data node stored in the project resources
 
-        Returns
-        -------
-        name : `str`
-            The unescaped name of an item
-        frequency : Clinical
-            The frequency of the node
-        ids : Dict[Clinical, str]
-            A dictionary of IDs for the node
-        """
-        ids = {}
-        id_parts = xname.split('___')[1:-1]
-        freq_value = 0b0
-        for part in id_parts:
-            layer_freq_str, id = part.split('__')
-            layer_freq = Clinical[layer_freq_str]
-            ids[layer_freq] = id
-            freq_value != layer_freq
-        frequency = Clinical(freq_value)
-        name_path = '/'.join(id_parts[-1].split('__'))
-        return name_path, frequency, ids
+    #     Returns
+    #     -------
+    #     name : `str`
+    #         The unescaped name of an item
+    #     frequency : Clinical
+    #         The frequency of the node
+    #     ids : Dict[Clinical, str]
+    #         A dictionary of IDs for the node
+    #     """
+    #     ids = {}
+    #     id_parts = xname.split('___')[1:-1]
+    #     freq_value = 0b0
+    #     for part in id_parts:
+    #         layer_freq_str, id = part.split('__')
+    #         layer_freq = Clinical[layer_freq_str]
+    #         ids[layer_freq] = id
+    #         freq_value != layer_freq
+    #     frequency = Clinical(freq_value)
+    #     name_path = '/'.join(id_parts[-1].split('__'))
+    #     return name_path, frequency, ids
 
     @classmethod
     def standard_uri(cls, xnode):
@@ -682,219 +681,6 @@ class Xnat(Repository):
             uri = re.sub(r'(?<=/subjects/)[^/]+', subject_id, uri)
 
         return uri
-
-
-    @classmethod
-    def make_command_json(cls, image_name, analysis_cls, inputs, outputs,
-                          parameters, desc, frequency=Clinical.session,
-                          docker_index="https://index.docker.io/v1/"):
-
-        if frequency != Clinical.session:
-            raise NotImplementedError(
-                "Support for frequencies other than '{}' haven't been "
-                "implemented yet".format(frequency))
-        try:
-            analysis_name, version = image_name.split('/')[1].split(':')
-        except (IndexError, ValueError):
-            raise ArcanaUsageError(
-                "The Docker organisation and tag needs to be provided as part "
-                "of the image, e.g. australianimagingservice/dwiqa:0.1")
-
-        cmd_inputs = []
-        input_names = []
-        for inpt in inputs:
-            input_name = inpt if isinstance(inpt, str) else inpt[0]
-            input_names.append(input_name)
-            spec = analysis_cls.data_spec(input_name)
-            desc = spec.desc if spec.desc else ""
-            if spec.is_file_group:
-                desc = ("Scan match: {} [SCAN_TYPE [ORDER [TAG=VALUE, ...]]]"
-                        .format(desc))
-            else:
-                desc = "Field match: {} [FIELD_NAME]".format(desc)
-            cmd_inputs.append({
-                "name": input_name,
-                "description": desc,
-                "type": "string",
-                "default-value": "",
-                "required": True,
-                "user-settable": True,
-                "replacement-key": "#{}_INPUT#".format(input_name.upper())})
-
-        for param in parameters:
-            spec = analysis_cls.param_spec(param)
-            desc = "Parameter: " + spec.desc
-            if spec.choices:
-                desc += " (choices: {})".format(','.join(spec.choices))
-
-            cmd_inputs.append({
-                "name": param,
-                "description": desc,
-                "type": COMMAND_INPUT_TYPES[spec.datatype],
-                "default-value": (spec.default
-                                    if spec.default is not None else ""),
-                "required": spec.default is None,
-                "user-settable": True,
-                "replacement-key": "#{}_PARAM#".format(param.upper())})
-
-        cmd_inputs.append(
-            {
-                "name": "project-id",
-                "description": "Project ID",
-                "type": "string",
-                "required": True,
-                "user-settable": False,
-                "replacement-key": "#PROJECT_ID#"
-            })
-
-
-        cmdline = (
-            "arcana derive /input {cls} {name} {derivs} {inputs} {params}"
-            " --scratch /work --repository xnat_cs #PROJECT_URI#"
-            .format(
-                cls='.'.join((analysis_cls.__module__, analysis_cls.__name__)),
-                name=analysis_name,
-                derivs=' '.join(outputs),
-                inputs=' '.join('-i {} #{}_INPUT#'.format(i, i.upper())
-                                for i in input_names),
-                params=' '.join('-p {} #{}_PARAM#'.format(p, p.upper())
-                                for p in parameters)))
-
-        if frequency == Clinical.session:
-            cmd_inputs.append(
-                {
-                    "name": "session-id",
-                    "description": "",
-                    "type": "string",
-                    "required": True,
-                    "user-settable": False,
-                    "replacement-key": "#SESSION_ID#"
-                })
-            cmdline += "#SESSION_ID# --session_ids #SESSION_ID# "
-
-        return {
-            "name": analysis_name,
-            "description": desc,
-            "label": analysis_name,
-            "version": version,
-            "schema-version": "1.0",
-            "image": image_name,
-            "index": docker_index,
-            "type": "docker",
-            "command-line": cmdline,
-            "override-entrypoint": True,
-            "mounts": [
-                {
-                    "name": "in",
-                    "writable": False,
-                    "name_path": "/input"
-                },
-                {
-                    "name": "output",
-                    "writable": True,
-                    "name_path": "/output"
-                },
-                {
-                    "name": "work",
-                    "writable": True,
-                    "name_path": "/work"
-                }
-            ],
-            "ports": {},
-            "inputs": cmd_inputs,
-            "outputs": [
-                {
-                    "name": "output",
-                    "description": "Derivatives",
-                    "required": True,
-                    "mount": "out",
-                    "name_path": None,
-                    "glob": None
-                },
-                {
-                    "name": "working",
-                    "description": "Working directory",
-                    "required": True,
-                    "mount": "work",
-                    "name_path": None,
-                    "glob": None
-                }
-            ],
-            "xnat": [
-                {
-                    "name": analysis_name,
-                    "description": desc,
-                    "contexts": ["xnat:imageSessionData"],
-                    "external-inputs": [
-                        {
-                            "name": "session",
-                            "description": "Imaging session",
-                            "type": "Session",
-                            "source": None,
-                            "default-value": None,
-                            "required": True,
-                            "replacement-key": None,
-                            "sensitive": None,
-                            "provides-value-for-command-input": None,
-                            "provides-files-for-command-mount": "in",
-                            "via-setup-command": None,
-                            "user-settable": None,
-                            "load-children": True
-                        }
-                    ],
-                    "derived-inputs": [
-                        {
-                            "name": "session-id",
-                            "type": "string",
-                            "required": True,
-                            "load-children": True,
-                            "derived-from-wrapper-input": "session",
-                            "derived-from-xnat-object-property": "id",
-                            "provides-value-for-command-input": "session-id"
-                        },
-                        {
-                            "name": "subject",
-                            "type": "Subject",
-                            "required": True,
-                            "user-settable": False,
-                            "load-children": True,
-                            "derived-from-wrapper-input": "session"
-                        },
-                        {
-                            "name": "project-id",
-                            "type": "string",
-                            "required": True,
-                            "load-children": True,
-                            "derived-from-wrapper-input": "subject",
-                            "derived-from-xnat-object-property": "id",
-                            "provides-value-for-command-input": "subject-id"
-                        }
-                    ],
-                    "output-handlers": [
-                        {
-                            "name": "output-resource",
-                            "accepts-command-output": "output",
-                            "via-wrapup-command": None,
-                            "as-a-child-of": "session",
-                            "type": "Resource",
-                            "label": "Derivatives",
-                            "format": None
-                        },
-                        {
-                            "name": "working-resource",
-                            "accepts-command-output": "working",
-                            "via-wrapup-command": None,
-                            "as-a-child-of": "session",
-                            "type": "Resource",
-                            "label": "Work",
-                            "format": None
-                        }
-                    ]
-                }
-            ]
-        }
-
-
 
 
 def append_suffix(path, suffix):
