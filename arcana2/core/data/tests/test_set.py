3d8eb6ae846c7ecc31a70dcdc9089f52f4cd5b9a
Tom Close <tom.g.close@gmail.com>
2021-10-13 14:39:15 +1100

    debugging test dataset pickle

--- arcana2/core/data/tests/test_set.py
+++ arcana2/core/data/tests/test_set.py
@@ -1,13 +1,16 @@
-import os.path
-from itertools import repeat
-import tempfile
-import pytest
+from pathlib import Path
 import cloudpickle as cp
+from arcana2.core.data.set import Dataset
 from arcana2.core.data.spec import DataSource, DataSink
 from arcana2.repositories.file_system import FileSystem
 from arcana2.dataspaces.clinical import Clinical as cl
 from arcana2.datatypes.neuroimaging import dicom, niftix_gz
 
 
-def test_dataset_pickle(dataset):
-   pass 
+def test_dataset_pickle(dataset: Dataset, tmp_dir: Path):
+    fpath = tmp_dir / 'dataset.pkl'
+    with fpath.open("wb") as fp:
+        cp.dump(dataset, fp)
+    with fpath.open("rb") as fp:
+        reloaded = cp.load(fp)
+    assert dataset == reloaded
