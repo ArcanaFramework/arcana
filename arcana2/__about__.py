9bedc180cdd19cc5ab4e42927c21a4f1719c09bf
Tom Close <tom.g.close@gmail.com>
2022-01-14 17:38:26 +1100

    swtiched pydra sub-modules to branches referencing serial-worker-fix until it is merged into a release

--- arcana2/__about__.py
+++ arcana2/__about__.py
@@ -8,8 +8,8 @@ __authors__ = [
 install_requires = [
     'xnat>=0.3.17',
     'pydra @ https://github.com/tclose/pydra/archive/serial-worker-fix.zip',
-    'pydra-dcm2niix',
-    'pydra-mrtrix3 @ https://github.com/nipype/pydra-mrtrix3/archive/master.zip',
+    'pydra-dcm2niix @ https://github.com/tclose/pydra-dcm2niix/archive/pydra-branch.zip',
+    'pydra-mrtrix3 @ https://github.com/tclose/pydra-mrtrix3/archive/pydra-branch.zip',
     'pydicom>=1.0.2',
     'nibabel>=3.2.1',
     'natsort>=7.1.1',
