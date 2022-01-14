039786088d1bd6995f143b2b40daa4288d6e8a1c
Tom Close <tom.g.close@gmail.com>
2022-01-14 16:10:36 +1100

    removed extras_require

--- arcana2/__about__.py
+++ arcana2/__about__.py
@@ -16,11 +16,12 @@ install_requires = [
     'fasteners>=0.7.0',
     'docker>=5.0.2',
     'neurodocker==0.7.0',
-    'deepdiff>=3.3',]
-
-tests_require = [
+    'deepdiff>=3.3',
+    # Tests
     'pytest>=5.4.3',
     'pytest-env>=0.6.2',
     'pytest-cov>=2.12.1',]
+
+tests_require = []
 
 python_versions = ['3.7', '3.8', '3.9']
