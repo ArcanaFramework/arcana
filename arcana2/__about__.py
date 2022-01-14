63e29d7534ac1c79aeebc202769230812ea5235d
Tom Close <tom.g.close@gmail.com>
2022-01-14 14:30:06 +1100

    switched pydra-mrtrix3 dependency to GH version

--- arcana2/__about__.py
+++ arcana2/__about__.py
@@ -8,21 +8,19 @@ __authors__ = [
 install_requires = [
     'xnat>=0.3.17',
     'pydra>=0.14.1',
+    'pydra-dcm2niix',
+    'pydra-mrtrix3 @ https://github.com/nipype/pydra-mrtrix3/archive/master.zip',
     'pydicom>=1.0.2',
     'nibabel>=3.2.1',
-    'pydra-mrtrix3',
-    'pydra-dcm2niix',
     'natsort>=7.1.1',
     'fasteners>=0.7.0',
     'docker>=5.0.2',
     'neurodocker==0.7.0',
-    'deepdiff>=3.3',
-    'pytest>=5.4.3',]
-
+    'deepdiff>=3.3',]
 
 tests_require = [
+    'pytest>=5.4.3',
     'pytest-env>=0.6.2',
-    'pytest-cov>=2.12.1',
-    'nibabel>=3.2.1']
+    'pytest-cov>=2.12.1',]
 
-python_versions = ['3.6', '3.7', '3.8', '3.9']
+python_versions = ['3.7', '3.8', '3.9']
