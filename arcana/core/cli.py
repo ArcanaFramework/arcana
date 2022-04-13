d0ffc598bb4db44bc52e83e4efcfc9a9345d288a
Tom Close <tom.g.close@gmail.com>
2022-04-13 15:05:00 +1000

    added version option

--- arcana/core/cli.py
+++ arcana/core/cli.py
@@ -1,10 +1,13 @@
 import click
+from arcana import __version__
 
 
 # Define the base CLI entrypoint
 @click.group()
+@click.version_option(version=__version__)
 def cli():
     pass
+
 
 # import sys
 # import logging
