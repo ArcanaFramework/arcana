01f76e48d2d65d487142c4c63482dcf25f56debf
Antoine Blachair <antoine.busy@gmail.com>
2022-04-28 09:33:33 +1000

    Add test for core.data.store.DataStore.save | multiple store name

--- arcana/core/data/tests/test_data_store.py
+++ arcana/core/data/tests/test_data_store.py
@@ -1,5 +1,23 @@
+import os
+from unittest.mock import patch
+
 from arcana.core.data.store import DataStore
+from arcana.exceptions import ArcanaNameError
+
 
 
 def test_singletons():
     assert sorted(DataStore.singletons()) == ['bids', 'file']    
+
+def test_store_save(xnat_repository, work_dir):
+    nickname1='store1'
+    nickname2='store2'
+    test_home_dir = work_dir / 'test-arcana-home'
+    # Create a new home directory so it doesn't conflict with user settings
+    with patch.dict(os.environ, {'ARCANA_HOME': str(test_home_dir)}):
+        store = xnat_repository
+        store.save(nickname1)
+        store.save(nickname2)
+        stores  = DataStore.load_saved_entries()
+        assert nickname1 in stores
+        assert nickname2 in stores
