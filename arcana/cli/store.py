c334268f387af08e2fdaa6b66dafc2fe6ff160ee
Antoine Blachair <antoine.busy@gmail.com>
2022-04-27 15:32:31 +1000

    Change to the recommended way to format Click help (https://click.palletsprojects.com/en/8.1.x/documentation/#documenting-arguments)

--- arcana/cli/store.py
+++ arcana/cli/store.py
@@ -49,14 +49,12 @@ def add(nickname, type, location, varargs, cache, user, password):
 
 @store.command(help="""
 Gives a data store saved in the config file ('~/.arcana/stores.yml') a new
-nickname
+nickname.
 
-Arguments\n
----------\n
-old_nickname
-    The current name of the store\n
-new_nickname
-    The new name for the store""")
+Arguments
+
+OLD_NICKNAME The current name of the store.
+NEW_NICKNAME The new name for the store.""")
 @click.argument('old_nickname')
 @click.argument('new_nickname')
 def rename(old_nickname, new_nickname):
