c4149747a35c504f52ac59e26a1e5b7eba11d95d
Antoine Blachair <antoine.busy@gmail.com>
2022-05-05 15:34:36 +1000

    Update cli store test following pylint recomendations

--- arcana/cli/tests/test_store_cli.py
+++ arcana/cli/tests/test_store_cli.py
@@ -1,5 +1,4 @@
 import os
-import pytest
 from unittest.mock import patch
 from arcana.cli.store import add, ls, remove, rename
 from arcana.test.utils import show_cli_trace
@@ -24,7 +23,7 @@ def test_store_cli(xnat_repository, cli_runner, work_dir):
         assert 'file - arcana.data.stores.common.file_system:FileSystem' in result.output
         assert 'test-xnat - arcana.data.stores.medimage.xnat.api:Xnat' in result.output
         assert '    server: ' + xnat_repository.server in result.output
-        
+
 
 def test_store_cli_remove(xnat_repository, cli_runner, work_dir):
     test_home_dir = work_dir / 'test-arcana-home'
@@ -82,8 +81,8 @@ def test_store_cli_encrypt_credentials(xnat_repository, cli_runner, work_dir):
         assert result.exit_code == 0, show_cli_trace(result)
         # Check credentials have been encrypted
         loaded_xnat_repository = DataStore.load('test-xnat')
-        assert loaded_xnat_repository.password is not ''
-        assert loaded_xnat_repository.password is not xnat_repository.password
-        assert loaded_xnat_repository.user is not ''
-        assert loaded_xnat_repository.user is not xnat_repository.user
+        assert loaded_xnat_repository.password != ''
+        assert loaded_xnat_repository.password != xnat_repository.password
+        assert loaded_xnat_repository.user != ''
+        assert loaded_xnat_repository.user != xnat_repository.user
 
