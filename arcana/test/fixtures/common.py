155f2a35b145c1392c3644179c55f3426fb94ccc
Antoine Blachair <antoine.busy@gmail.com>
2022-05-06 15:06:03 +1000

    Format and lint common.py (fixtures)

--- arcana/test/fixtures/common.py
+++ arcana/test/fixtures/common.py
@@ -4,135 +4,173 @@ import shutil
 import pytest
 from arcana.data.formats.common import Text, Directory, Json
 from arcana.data.formats.medimage import (
-    NiftiGz, NiftiGzX, NiftiX, Nifti, Analyze, MrtrixImage)
-from pathlib import Path
-import pytest
+    NiftiGz,
+    NiftiGzX,
+    NiftiX,
+    Nifti,
+    Analyze,
+    MrtrixImage,
+)
 from arcana.test.tasks import (
-    add, path_manip, attrs_func, A, B, C, concatenate, concatenate_reverse)
+    add,
+    path_manip,
+    attrs_func,
+    A,
+    B,
+    C,
+    concatenate,
+    concatenate_reverse,
+)
 from arcana.test.datasets import (
-    TestDatasetBlueprint, TestDataSpace as TDS, Xyz, make_dataset)
+    TestDatasetBlueprint,
+    TestDataSpace as TDS,
+    Xyz,
+    make_dataset,
+)
 
 
 TEST_TASKS = {
-    'add': (add,
-            {'a': 4,
-             'b': 5},
-            {'out': 9}),
-    'path_manip': (path_manip,
-                   {'dpath': Path('/home/foo/Desktop'),
-                    'fname': 'bar.txt'},
-                   {'path': '/home/foo/Desktop/bar.txt',
-                    'suffix': '.txt'}),
-    'attrs_func': (attrs_func, 
-                   {'a': A(x=2, y=4),
-                    'b': B(u=2.5, v=1.25)},
-                   {'c': C(z=10)})}
+    "add": (add, {"a": 4, "b": 5}, {"out": 9}),
+    "path_manip": (
+        path_manip,
+        {"dpath": Path("/home/foo/Desktop"), "fname": "bar.txt"},
+        {"path": "/home/foo/Desktop/bar.txt", "suffix": ".txt"},
+    ),
+    "attrs_func": (
+        attrs_func,
+        {"a": A(x=2, y=4), "b": B(u=2.5, v=1.25)},
+        {"c": C(z=10)},
+    ),
+}
 
-BASIC_TASKS = ['add', 'path_manip', 'attrs_func']
+BASIC_TASKS = ["add", "path_manip", "attrs_func"]
 
-FILE_TASKS = ['concatenate']
+FILE_TASKS = ["concatenate"]
 
 
 @pytest.fixture(params=BASIC_TASKS)
 def pydra_task_details(request):
     func_name = request.param
-    return ('arcana.tasks.tests.fixtures' + func_name,) + tuple(
-        TEST_TASKS[func_name][1:])
-            
+    return ("arcana.tasks.tests.fixtures" + func_name,) + tuple(
+        TEST_TASKS[func_name][1:]
+    )
+
 
 @pytest.fixture(params=BASIC_TASKS)
 def pydra_task(request):
-    task, args, expected_out = TEST_TASKS[request.param]
+    task, args = TEST_TASKS[request.param]
     task.test_args = args  # stash args away in task object for future access
     return task
 
 
 TEST_DATASET_BLUEPRINTS = {
-    'full' : TestDatasetBlueprint(  # dataset name
+    "full": TestDatasetBlueprint(  # dataset name
         [TDS.a, TDS.b, TDS.c, TDS.d],
         [2, 3, 4, 5],
-        ['file1.txt', 'file2.nii.gz', 'dir1'],
+        ["file1.txt", "file2.nii.gz", "dir1"],
         [],
-        {'file1': [
-            (Text, ['file1.txt'])],
-         'file2': [
-            (NiftiGz, ['file2.nii.gz'])],
-         'dir1': [
-            (Directory, ['dir1'])]},
-        [('deriv1', TDS.abcd, Text, ['file1.txt']),  # Derivatives to insert
-         ('deriv2', TDS.c, Directory, ['dir']),
-         ('deriv3', TDS.bd, Text, ['file1.txt'])]
+        {
+            "file1": [(Text, ["file1.txt"])],
+            "file2": [(NiftiGz, ["file2.nii.gz"])],
+            "dir1": [(Directory, ["dir1"])],
+        },
+        [
+            ("deriv1", TDS.abcd, Text, ["file1.txt"]),  # Derivatives to insert
+            ("deriv2", TDS.c, Directory, ["dir"]),
+            ("deriv3", TDS.bd, Text, ["file1.txt"]),
+        ],
     ),
-    'one_layer': TestDatasetBlueprint(
+    "one_layer": TestDatasetBlueprint(
         [TDS.abcd],
         [1, 1, 1, 5],
-        ['file1.nii.gz', 'file1.json', 'file2.nii', 'file2.json'],
+        ["file1.nii.gz", "file1.json", "file2.nii", "file2.json"],
         [],
-        {'file1': [
-            (NiftiGzX, ['file1.nii.gz', 'file1.json']),
-            (NiftiGz, ['file1.nii.gz']),
-            (Json, ['file1.json'])],
-         'file2': [
-            (NiftiX, ['file2.nii', 'file2.json']),
-            (Nifti, ['file2.nii']),
-            (Json, ['file2.json'])]},
-        [('deriv1', TDS.abcd, Json, ['file1.json']),
-         ('deriv2', TDS.bc, Xyz, ['file1.x', 'file1.y', 'file1.z']),
-         ('deriv3', TDS._, MrtrixImage, ['file1.mif'])]
+        {
+            "file1": [
+                (NiftiGzX, ["file1.nii.gz", "file1.json"]),
+                (NiftiGz, ["file1.nii.gz"]),
+                (Json, ["file1.json"]),
+            ],
+            "file2": [
+                (NiftiX, ["file2.nii", "file2.json"]),
+                (Nifti, ["file2.nii"]),
+                (Json, ["file2.json"]),
+            ],
+        },
+        [
+            ("deriv1", TDS.abcd, Json, ["file1.json"]),
+            ("deriv2", TDS.bc, Xyz, ["file1.x", "file1.y", "file1.z"]),
+            ("deriv3", TDS._, MrtrixImage, ["file1.mif"]),
+        ],
     ),
-    'skip_single': TestDatasetBlueprint(
+    "skip_single": TestDatasetBlueprint(
         [TDS.a, TDS.bc, TDS.d],
         [2, 1, 2, 3],
-        ['doubledir1', 'doubledir2'],
+        ["doubledir1", "doubledir2"],
         [],
-        {'doubledir1': [
-            (Directory, ['doubledir1'])],
-         'doubledir2': [
-            (Directory, ['doubledir2'])]},
-        [('deriv1', TDS.ad, Json, ['file1.json'])]
+        {
+            "doubledir1": [(Directory, ["doubledir1"])],
+            "doubledir2": [(Directory, ["doubledir2"])],
+        },
+        [("deriv1", TDS.ad, Json, ["file1.json"])],
     ),
-    'skip_with_inference': TestDatasetBlueprint(
+    "skip_with_inference": TestDatasetBlueprint(
         [TDS.bc, TDS.ad],
         [2, 3, 2, 4],
-        ['file1.img', 'file1.hdr', 'file2.mif'],
-        [(TDS.bc, r'b(?P<b>\d+)c(?P<c>\d+)'),
-         (TDS.ad, r'a(?P<a>\d+)d(?P<d>\d+)')],
-        {'file1': [
-            (Analyze, ['file1.hdr', 'file1.img'])],
-         'file2': [
-            (MrtrixImage, ['file2.mif'])]},
-        []
+        ["file1.img", "file1.hdr", "file2.mif"],
+        [(TDS.bc, r"b(?P<b>\d+)c(?P<c>\d+)"), (TDS.ad, r"a(?P<a>\d+)d(?P<d>\d+)")],
+        {
+            "file1": [(Analyze, ["file1.hdr", "file1.img"])],
+            "file2": [(MrtrixImage, ["file2.mif"])],
+        },
+        [],
     ),
-    'redundant': TestDatasetBlueprint(
-        [TDS.abc, TDS.abcd],  # e.g. XNAT where session ID is unique in project but final layer is organised by timepoint
+    "redundant": TestDatasetBlueprint(
+        [
+            TDS.abc,
+            TDS.abcd,
+        ],  # e.g. XNAT where session ID is unique in project but final layer is organised by timepoint
         [3, 4, 5, 6],
-        ['doubledir', 'file1.x', 'file1.y', 'file1.z'],
-        [(TDS.abc, r'a(?P<a>\d+)b(?P<b>\d+)c(?P<c>\d+)'),
-         (TDS.abcd, r'a\d+b\d+c\d+d(?P<d>\d+)')],
-        {'doubledir': [
-            (Directory, ['doubledir'])],
-         'file1': [
-            (Xyz, ['file1.x', 'file1.y', 'file1.z'])]},
-        [('deriv1', TDS.d, Json, ['file1.json'])]
+        ["doubledir", "file1.x", "file1.y", "file1.z"],
+        [
+            (TDS.abc, r"a(?P<a>\d+)b(?P<b>\d+)c(?P<c>\d+)"),
+            (TDS.abcd, r"a\d+b\d+c\d+d(?P<d>\d+)"),
+        ],
+        {
+            "doubledir": [(Directory, ["doubledir"])],
+            "file1": [(Xyz, ["file1.x", "file1.y", "file1.z"])],
+        },
+        [("deriv1", TDS.d, Json, ["file1.json"])],
     ),
-    'concatenate_test': TestDatasetBlueprint(
-        [TDS.abcd],  # e.g. XNAT where session ID is unique in project but final layer is organised by timepoint
+    "concatenate_test": TestDatasetBlueprint(
+        [
+            TDS.abcd
+        ],  # e.g. XNAT where session ID is unique in project but final layer is organised by timepoint
         [1, 1, 1, 2],
-        ['file1.txt', 'file2.txt'],
-        {}, {}, []),
-    'concatenate_zip_test': TestDatasetBlueprint(
-        [TDS.abcd],  # e.g. XNAT where session ID is unique in project but final layer is organised by timepoint
+        ["file1.txt", "file2.txt"],
+        {},
+        {},
+        [],
+    ),
+    "concatenate_zip_test": TestDatasetBlueprint(
+        [
+            TDS.abcd
+        ],  # e.g. XNAT where session ID is unique in project but final layer is organised by timepoint
         [1, 1, 1, 1],
-        ['file1.zip', 'file2.zip'],
-        {}, {}, [])}
+        ["file1.zip", "file2.zip"],
+        {},
+        {},
+        [],
+    ),
+}
 
 
-GOOD_DATASETS = ['full', 'one_layer', 'skip_single', 'skip_with_inference',
-                 'redundant']
+GOOD_DATASETS = ["full", "one_layer", "skip_single", "skip_with_inference", "redundant"]
 
 # ------------------------------------
 # Pytest fixtures and helper functions
 # ------------------------------------
+
 
 @pytest.fixture
 def test_dataspace():
@@ -141,12 +179,12 @@ def test_dataspace():
 
 @pytest.fixture
 def test_dataspace_location():
-    return __name__ + '.TestDataSpace'
+    return __name__ + ".TestDataSpace"
 
 
 @pytest.fixture
 def test_dicom_dataset_dir(test_ref_data_dir):
-    return Path(__file__).parent / 'test-dataset'
+    return Path(__file__).parent / "test-dataset"
 
 
 @pytest.fixture(params=GOOD_DATASETS)
@@ -154,76 +192,83 @@ def dataset(work_dir, request):
     dataset_name = request.param
     blueprint = TEST_DATASET_BLUEPRINTS[dataset_name]
     dataset_path = work_dir / dataset_name
-    dataset = make_dataset(blueprint, dataset_path)
-    yield dataset
-    #shutil.rmtree(dataset.id)
+    dataset2 = make_dataset(blueprint, dataset_path)
+    yield dataset2
+    # shutil.rmtree(dataset.id)
 
 
 @pytest.fixture
 def saved_dataset(work_dir):
 
     blueprint = TestDatasetBlueprint(
-        [TDS.abcd],  # e.g. XNAT where session ID is unique in project but final layer is organised by timepoint
+        [
+            TDS.abcd
+        ],  # e.g. XNAT where session ID is unique in project but final layer is organised by timepoint
         [1, 1, 1, 1],
-        ['file1.txt', 'file2.txt'],
-        {}, {}, [])
+        ["file1.txt", "file2.txt"],
+        {},
+        {},
+        [],
+    )
 
-    dataset_path = work_dir / 'saved_dataset'
-    dataset = make_dataset(blueprint, dataset_path)
-    dataset.save()
-    return dataset
+    dataset_path = work_dir / "saved_dataset"
+    dataset2 = make_dataset(blueprint, dataset_path)
+    dataset2.save()
+    return dataset2
 
 
 @pytest.fixture
 def tmp_dir():
-    tmp_dir = Path(mkdtemp())
-    yield tmp_dir
-    shutil.rmtree(tmp_dir)
+    tmp_dir2 = Path(mkdtemp())
+    yield tmp_dir2
+    shutil.rmtree(tmp_dir2)
 
 
-@pytest.fixture(params=['forward', 'reverse'])
+@pytest.fixture(params=["forward", "reverse"])
 def concatenate_task(request):
-    if request.param == 'forward':
+    if request.param == "forward":
         task = concatenate
-        task.__name__ = 'concatenate'  # FIXME: Can be removed after https://github.com/nipype/pydra/pull/533 is merged
+        task.__name__ = "concatenate"  # FIXME: Can be removed after https://github.com/nipype/pydra/pull/533 is merged
     else:
         task = concatenate_reverse
     return task
 
+
 @pytest.fixture
 def command_spec():
     return {
-        'name': 'conctenate-test',
-        'pydra_task': 'arcana.test.tasks:concatenate',
-        'inputs': [
+        "name": "conctenate-test",
+        "pydra_task": "arcana.test.tasks:concatenate",
+        "inputs": [
             {
-                'pydra_field': 'in_file1',
-                'format': 'common:Text',
-                'xnat_name': 'first-file',
-                'frequency': 'session'
+                "pydra_field": "in_file1",
+                "format": "common:Text",
+                "xnat_name": "first-file",
+                "frequency": "session",
             },
             {
-                'pydra_field': 'in_file2',
-                'format': 'common:Text',
-                'xnat_name': 'second-file',
-                'frequency': 'session'
+                "pydra_field": "in_file2",
+                "format": "common:Text",
+                "xnat_name": "second-file",
+                "frequency": "session",
             },
         ],
-        'outputs': [
+        "outputs": [
             {
-                'pydra_field': 'out_file',
-                'format': 'common:Text',
-                'xnat_path': 'concatenated'
+                "pydra_field": "out_file",
+                "format": "common:Text",
+                "xnat_path": "concatenated",
             }
         ],
-        'parameters': [
+        "parameters": [
             {
-                'pydra_field': 'duplicates',
-                'xnat_name': 'number-of-duplicates',
-                'required': True
+                "pydra_field": "duplicates",
+                "xnat_name": "number-of-duplicates",
+                "required": True,
             }
         ],
-        'description': "A pipeline to test Arcana's deployment tool",
-        'version': '0.1',
-        'frequency': 'session',
-        'info_url': None}
+        "description": "A pipeline to test Arcana's deployment tool",
+        "version": "0.1",
+        "frequency": "session",
+        "info_url": None,
+    }
