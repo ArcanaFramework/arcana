52ffbd2fc744d71c66b2f5153b7b5619639d62ff
Tom Close <tom.g.close@gmail.com>
2022-06-02 20:53:50 +1000

    changed Pydra dependencies to work with patched Pydra branch

--- arcana/__about__.py
+++ arcana/__about__.py
@@ -8,7 +8,7 @@ __authors__ = [
 
 install_requires = [
     'xnat>=0.3.17',
-    'pydra==0.18',
+    'pydra @ git+https://github.com/tclose/pydra.git@rebased-shlex-split',
     'pydra-dcm2niix>=1.0.0rc2',
     'pydra-mrtrix3>=0.1',
     'pydicom>=1.0.2',
