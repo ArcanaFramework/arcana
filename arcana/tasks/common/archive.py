82fd1489846627fa26fb08a1e858ba503979b2c9
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-28 17:31:01 +0000

    Adding tarfile member sanitization to extractall()

--- arcana/tasks/common/archive.py
+++ arcana/tasks/common/archive.py
@@ -89,7 +89,26 @@ def extract_tar(
         compression_type = ""
 
     with tarfile.open(in_file, mode=f"r:{compression_type}") as tfile:
-        tfile.extractall(path=extract_dir)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tfile, path=extract_dir)
 
     return [os.path.join(extract_dir, f) for f in os.listdir(extract_dir)]
 
