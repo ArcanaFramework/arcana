8dec57077bdf564edde52e4e01f4002cf205dc97
Tom Close <tom.g.close@gmail.com>
2023-01-24 11:00:14 +1030

    moved in functionality from fileformats

--- arcana/core/data/cell.py
+++ arcana/core/data/cell.py
@@ -1,11 +1,15 @@
 from __future__ import annotations
 import typing as ty
+from pathlib import Path
+import hashlib
 import attrs
 from arcana.core.exceptions import (
     NameError,
-    DataNotDerivedYetError,
 )
 from .quality import DataQuality  # @ignore reshadowedImports
+from arcana.core.utils.misc import (
+    HASH_CHUNK_SIZE,
+)
 
 if ty.TYPE_CHECKING:
     from .row import DataRow
@@ -57,6 +61,10 @@ class DataCell:
     _checksums: ty.Dict[str, str] = attrs.field(default=None, repr=False, init=False)
 
     @property
+    def datatype(self):
+        return self.column.datatype
+
+    @property
     def recorded_checksums(self):
         if self.provenance is None:
             return None
@@ -67,24 +75,187 @@ class DataCell:
     def check_provenance(self, _, provenance):
         "Checks that the data item path is present in the provenance"
         if provenance is not None:
-            if self.path not in provenance.outputs:
+            if self.id not in provenance.outputs:
                 raise NameError(
-                    self.path,
-                    f"{self.path} was not found in outputs "
+                    self.id,
+                    f"{self.id} was not found in outputs "
                     f"{provenance.outputs.keys()} of provenance provenance "
                     f"{provenance}",
                 )
 
-    def _check_exists(self):
-        if not self.exists:
-            raise DataNotDerivedYetError(
-                self.path, f"Cannot access {self} as it hasn't been derived yet"
-            )
+    @property
+    def checksums(self):
+        if self._checksums is None:
+            self.get_checksums()
+        return self._checksums
 
-    def _check_part_of_row(self):
-        if self.row is None:
-            raise RuntimeError(f"Cannot 'get' {self} as it is not part of a dataset")
+    def get_checksums(self, force_calculate=False):
+        self._check_exists()
+        # Load checksums from store (e.g. via API)
+        if self.row is not None and not force_calculate:
+            self._checksums = self.row.dataset.store.get_checksums(self)
+        # If the store cannot calculate the checksums do them manually
+        else:
+            self._checksums = self.calculate_checksums()
 
-    @classmethod
-    def class_name(cls):
-        return cls.__name__.lower()
+    def calculate_checksums(self):
+        self._check_exists()
+        checksums = {}
+        for fpath in self.all_file_paths():
+            fhash = hashlib.md5()
+            with open(fpath, "rb") as f:
+                # Calculate hash in chunks so we don't run out of memory for
+                # large files.
+                for chunk in iter(lambda: f.read(HASH_CHUNK_SIZE), b""):
+                    fhash.update(chunk)
+            checksums[fpath] = fhash.hexdigest()
+        checksums = self.generalise_checksum_keys(checksums)
+        return checksums
+
+    def generalise_checksum_keys(
+        self, checksums: ty.Dict[str, str], base_path: Path = None
+    ):
+        """Generalises the paths used for the file paths in a checksum dictionary
+        so that they are the same irrespective of that the top-level file-system
+        paths are
+
+        Parameters
+        ----------
+        checksums: dict[str, str]
+            The checksum dict mapping relative file paths to checksums
+
+        Returns
+        -------
+        dict[str, str]
+            The checksum dict with file paths generalised"""
+        if base_path is None:
+            base_path = self.fspath
+        return {str(Path(k).relative_to(base_path)): v for k, v in checksums.items()}
+
+    def contents_equal(self, other, **kwargs):
+        """
+        Test the equality of the fileset contents with another fileset.
+        If the fileset's format implements a 'contents_equal' method than
+        that is used to determine the equality, otherwise a straight comparison
+        of the checksums is used.
+
+        Parameters
+        ----------
+        other : FileSet
+            The other fileset to compare to
+        """
+        return self.checksums == other.checksums
+
+    # @classmethod
+    # def resolve(cls, unresolved):
+    #     """Resolve file set loaded from a repository to the specific datatype
+
+    #     Parameters
+    #     ----------
+    #     unresolved : UnresolvedFileSet
+    #         A file set loaded from a repository that has not been resolved to
+    #         a specific datatype yet
+
+    #     Returns
+    #     -------
+    #     FileSet
+    #         The resolved file-set object
+
+    #     Raises
+    #     ------
+    #     ArcanaUnresolvableFormatException
+    #         If there doesn't exist a unique resolution from the unresolved file
+    #         group to the given datatype, then an FileFormatError should be
+    #         raised
+    #     """
+    #     # Perform matching based on resource names in multi-datatype
+    #     # file-set
+    #     if unresolved.uris is not None:
+    #         item = None
+    #         for format_name, uri in unresolved.uris.items():
+    #             if cls.matches_format_name(format_name):
+    #                 item = cls(uri=uri, **unresolved.item_kwargs)
+    #         if item is None:
+    #             raise FileFormatError(
+    #                 f"Could not file a matching resource in {unresolved.path} for"
+    #                 f" the given datatype ({cls.class_name()}), found "
+    #                 "('{}')".format("', '".join(unresolved.uris))
+    #             )
+    #     else:
+    #         item = cls(**unresolved.item_kwargs)
+    #         item.set_fspaths(unresolved.file_paths)
+    #     return item
+
+    # def set_fspaths(self, fspaths: list[Path]):
+    #     """Set the file paths of the file set
+
+    #     Parameters
+    #     ----------
+    #     fspaths : list[Path]
+    #         The candidate paths from which to set the paths of the
+    #         file set from. Note that not all paths need to be set if
+    #         they are not relevant.
+
+    #     Raises
+    #     ------
+    #     FileFormatError
+    #         is raised if the required the paths cannot be set from the provided
+    #     """
+
+    # @classmethod
+    # def from_fspaths(cls, *fspaths: ty.List[Path], path=None):
+    #     """Create a FileSet object from a set of file-system paths
+
+    #     Parameters
+    #     ----------
+    #     fspaths : list[Path]
+    #         The candidate paths from which to set the paths of the
+    #         file set from. Note that not all paths need to be set if
+    #         they are not relevant.
+    #     path : str, optional
+    #         the location of the file-set relative to the node it (will)
+    #         belong to. Defaults to
+
+    #     Returns
+    #     -------
+    #     FileSet
+    #         The created file-set
+    #     """
+    #     if path is None:
+    #         path = fspaths[0].stem
+    #     obj = cls(path)
+    #     obj.set_fspaths(fspaths)
+    #     return obj
+
+    # def get(self, assume_exists=False):
+    #     if assume_exists:
+    #         self.exists = True
+    #     self._check_part_of_row()
+    #     fspaths = self.row.dataset.store.get_fileset_paths(self)
+    #     self.exists = True
+    #     self.set_fspaths(fspaths)
+    #     self.validate_file_paths()
+
+    # def put(self, *fspaths):
+    #     self._check_part_of_row()
+    #     fspaths = [Path(p) for p in fspaths]
+    #     dir_paths = list(p for p in fspaths if p.is_dir())
+    #     if len(dir_paths) > 1:
+    #         dir_paths_str = "', '".join(str(p) for p in dir_paths)
+    #         raise FileFormatError(
+    #             f"Cannot put more than one directory, {dir_paths_str}, as part "
+    #             f"of the same file set {self}"
+    #         )
+    #     # Make a copy of the file-set to validate the local paths and auto-gen
+    #     # any defaults before they are pushed to the store
+    #     cpy = copy(self)
+    #     cpy.exists = True
+    #     cpy.set_fspaths(fspaths)
+    #     cache_paths = self.row.dataset.store.put_fileset_paths(self, cpy.fspaths)
+    #     # Set the paths to the cached files
+    #     self.exists = True
+    #     self.set_fspaths(cache_paths)
+    #     self.validate_file_paths()
+    #     # Save provenance
+    #     if self.provenance:
+    #         self.row.dataset.store.put_provenance(self)
