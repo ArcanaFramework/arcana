a8afa242c461676e7c2e9dc15f924ab5836b0858
Tom Close <tom.g.close@gmail.com>
2023-03-20 13:36:57 +1100

    added debugging logs

--- arcana/testing/data/blueprint.py
+++ arcana/testing/data/blueprint.py
@@ -56,11 +56,13 @@ class EntryBlueprint(metaclass=ABCMeta):
         if self.ids and row.id not in self.ids:
             return
         item = self.make_item(**kwargs)
+        logger.debug("Creating entry at %s in %s", self.path, row)
         entry = row.dataset.store.create_entry(
             path=self.path,
             datatype=self.datatype,
             row=row,
         )
+        logger.debug("Putting %s at %s", item, entry)
         row.dataset.store.put(item, entry)
 
 
@@ -143,7 +145,9 @@ class FileSetEntryBlueprint(EntryBlueprint):
                         zfile.write(next_path)
                     (tmp_dir / next_path).unlink()
             out_paths.append(out_path)
-        return self.datatype(out_paths)
+        item = self.datatype(out_paths)
+        logger.debug("Created %s item", item)
+        return item
 
 
 @attrs.define(kw_only=True)
@@ -205,6 +209,9 @@ class TestDatasetBlueprint:
         orig_type = metadata.get("type", "test")
         metadata["type"] = "in-construction"
         with store.connection:
+            logger.debug(
+                "Creating test dataset in %s at %s from %s", store, dataset_id, self
+            )
             dataset = store.create_dataset(
                 id=dataset_id,
                 leaves=self.all_ids,
@@ -215,11 +222,13 @@ class TestDatasetBlueprint:
                 metadata=metadata,
                 **kwargs,
             )
+            logger.debug("Adding entries to test dataset")
             for row in dataset.rows(frequency=max(self.space)):
                 self.make_entries(row, source_data=source_data)
             dataset.metadata.type = orig_type
             dataset.save()
         dataset.__annotations__["blueprint"] = self
+        logger.debug("Successfully created test dataset at %s in %s", dataset_id, store)
         return dataset
 
     def translate_to(self, data_store: DataStore) -> "TestDatasetBlueprint":
