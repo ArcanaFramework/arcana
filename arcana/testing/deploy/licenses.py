ef4f323cc3c551b0b23cd8fbc36e44f1b5809b72
Tom Close <tom.g.close@gmail.com>
2023-08-08 12:11:32 +1000

    reworked structure of app specs

--- arcana/testing/deploy/licenses.py
+++ arcana/testing/deploy/licenses.py
@@ -10,7 +10,7 @@ def get_pipeline_image(license_path, app_cls=App) -> App:
         version="1.0",
         authors=[{"name": "Some One", "email": "some.one@an.email.org"}],
         info_url="http://concatenate.readthefakedocs.io",
-        description="A test of the license installation",
+        title="A test of the license installation",
         readme="This is a test README",
         packages={
             "pip": ["fileformats"],
